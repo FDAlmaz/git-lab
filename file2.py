6080b996e262a55a623c88c1a24accf5f12b22bd
FDAlmaz <128253696+FDAlmaz@users.noreply.github.com>
2024-09-24 00:37:46 +0300

    Update file2.py

--- file2.py
+++ file2.py
@@ -2,5 +2,5 @@ a=5
 b=6
 num1 = int(input("Введите первое целое число: "))
 num2 = int(input("Введите второе целое число: "))
-sum_result = num1 + num2
-print(f" {num1} + {num2} = {sum_result}.")
+sum_result = num1 - num2
+print(f" {num1} - {num2} = {sum_result}.")
